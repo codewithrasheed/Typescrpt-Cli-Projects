cbed04ef2b7fcfff331681d51b080e2b1b267d9d
Codewithrasheed <rasheedahmed4845@gmail.com>
2024-04-21 14:54:12 +0500

    simple-calculator

---
+++ Simple Calculator/index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let asnwer = await inquirer.prompt([
+    {
+        message: "Enter First Number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter Second Number",
+        type: "number   ",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of the fnollowing operators to perform the operation",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division"
+        ]
+    }
+]);
+
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber)
+}
+else if(asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber)
+}
+else if(asnwer.operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber)
+}
+else if(asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber)
+}
+else{
+    console.log("Please Enter Valid Number to perform operation!") 
+}
