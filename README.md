43ee1db81fa839f2f12e26a453b327590c3a4e88
Codewithrasheed <rasheedahmed4845@gmail.com>
2024-06-29 02:24:33 +0500

    Todo List App

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ### In this Repo I have uploaded all Typescript-cli-based projects using Typescript, Node.js, and Inquirer.
 #### First Project: Simple Calculator: npm command => npx rasheed985-simple-calculator
 #### Second Project: Number Guessing Game: npm command => npx rasheed65-number-guessing-game
+#### Third Project: ATM Machine: npm command => npx rasheed-atm-machine
+#### Fourth Project: Todo List App: npm command => npx rasheed-ahmed-todo-list
---
+++ ToDo List/main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let Tasks = [];
+let condition = true;
+
+while(condition){
+    let addTodo = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter a task: "
+        },
+        {
+            name: "action",
+            type: "confirm",
+            message: "Do you want to add more tasks: ",
+            default: false
+        },
+    ]);
+    Tasks.push(addTodo.task);
+    condition = addTodo.action;
+    console.log(Tasks);
+}
